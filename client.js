6f4d07213a653a9a52d4ea2ebba71a2a9f30a74d
kznrluk <kznr.luk@gmail.com>
2018-01-03 02:51:10 +0900

    Socket.io/HTTP経由でのファイルの受信

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
-var socket = require('socket.io-client').connect('http://localhost:3000');
+var socket = require('socket.io-client').connect('http://192.168.213.28:3000');
+var fs = require('fs');
+var request = require('request');
 
 socket.on('connect', function(){
     console.log('Connected');
@@ -6,7 +8,26 @@ socket.on('connect', function(){
 socket.on('message', function(data){
     console.log(data.msg);
 });
+
+socket.on('mp3data', function(data){
+    var end = (new Date()).getTime();
+    var sec = (end - data.start) / 1000;
+    var bytesPerSec = Math.round(data.mp3.length / sec);
+    console.log((bytesPerSec * 8 / 1000 / 1000) + 'Mbps');
+});
+
+function getwithHttp(){
+    var start = (new Date()).getTime();
+    request('http://localhost:3000/', function(err, res, body){
+        var end = (new Date()).getTime();
+        var sec = (end - start) / 1000;
+        var bytesPerSec = Math.round(body.length / sec);
+        console.log((bytesPerSec * 8 / 1000 / 1000) + 'Mbps');
+    });
+};
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', function(data){
-    socket.emit('message', {msg: data});
+    //getwithHttp();
+    socket.emit('starttest', {msg: data});
 });
