1df4bacc05c9bccb7f862da4d75e5aeed4f32592
kznrluk <kznr.luk@gmail.com>
2018-05-02 20:47:36 +0900

    ファイル読み取りをreadFileSyncに変更

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ const server = require('http').createServer();
 const io     = require('socket.io')(server);
 const fs     = require('fs');
 
-const FILE_NAME = 'sample.mp3'
-
-process.stdin.setEncoding('utf-8');
-
-var binaryData = 0;
-fs.readFile(FILE_NAME, (err, data) => {
-    binaryData = data;
-    console.log('File loaded.');
-});
+const FILE_PASS = 'sample.mp3';
+const binaryData = fs.readFileSync(FILE_PASS);
 
 io.on('connection', socket => {
     console.log('Connected.')
@@ -18,5 +11,6 @@ io.on('connection', socket => {
         io.emit('sendBinaryData', { 'binary': binaryData, 'startTime': (new Date()).getTime() });
     });
 });
+
 server.listen(3000);
 console.log('Server started on 3000');
